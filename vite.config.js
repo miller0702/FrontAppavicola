94307c126b57f5c112424411f3a2c7bb49ff1bbd
Miller Alvarez <mdalvarezb@ufpso.edu.co>
2024-07-17 20:22:19 -0500

    sexto commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@mui/x-date-pickers': '@mui/x-date-pickers/LocalizationProvider',
+    },
+  },
+});
